118319da85ffb885df9953151d0081cf4170c915
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-05-10 16:01:33 -0300

    conselho chines, primeira versao

---
+++ expressoes/conselho_chines.c
@@ -0,0 +1,65 @@
+/* Everaldo Gomes - 10/05/2012
+
+Conselho Chinês:
+
+Há apenas duas coisas com que você deve se preocupar:
+se você está bem ou se está doente.
+Se você está bem, não há nada com que se preocupar.
+Se você está doente, há duas coisas com que se preocupar:
+se você vai se curar ou se vai morrer.
+Se você vai morrer, há duas coisas com que se preocupar:
+se você vai para o céu ou para o inferno.
+Se vai para o céu, não há nada com que se preocupar.
+Agora, se você vai para o inferno, estará tão ocupado em
+cumprimentar velhos amigos que nem terá tempo de se preocupar.
+Então, para que se preocupar?
+
+
+
+
+*/
+
+#include <stdio.h>
+
+
+
+int main(){
+
+  int doente, doenca_fatal, malvado;
+
+  //Apesar de estar dividida em múltiplas linhas o texto abaixo forma apenas uma string!!!
+  printf("Há apenas duas coisas com que você deve se preocupar:\n"
+          "se você está bem ou se está doente.\n"
+          "Se você está bem, não há nada com que se preocupar.\n"
+          "Se você está doente, há duas coisas com que se preocupar:\n"
+          "se você vai se curar ou se vai morrer.\n"
+          "Se você vai morrer, há duas coisas com que se preocupar:\n"
+          "se você vai para o céu ou para o inferno.\n"
+          "Se vai para o céu, não há nada com que se preocupar.\n"
+          "Agora, se você vai para o inferno, estará tão ocupado em\n"
+          "cumprimentar velhos amigos que nem terá tempo de se preocupar.\n"
+          "Então, para que se preocupar?\n\n");
+
+  printf("Responda 1 para sim ou 0 para não\n");
+
+  printf("Você está doente?\n");
+  scanf("%d", &doente);
+
+  if (doente) {
+    printf("Você vai morrer?\n");
+    scanf("%d", &doenca_fatal);
+    if (doenca_fatal){
+        printf("Você vai para o inferno?\n");
+        scanf("%d", &malvado);
+        if (malvado){
+          printf("Você estava doente, morreu e foi para o inferno. Cumprimente os velhos amigos.\n");
+        }
+    }
+  }
+  printf("Não há com que se preocupar\n");
+
+
+
+
+
+}
