63f5eea5586b1086ebf50d09cce19bf66ddd68a8
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-05-24 15:58:41 -0300

    operador not

---
+++ estrutura/operadores/logicos/not.c
@@ -0,0 +1,16 @@
+/* Everaldo Gomes - 24/05/2012 */
+
+#include <stdio.h>
+
+int main(){
+
+  printf("%d\n", !0); //imprime 1
+  printf("%d\n", !1); //imprime 0
+  printf("%d\n", !33);//imprime 0
+  printf("%d\n", !22);//imprime 0
+  printf("%d\n", !74);//imprime 0
+
+
+
+  return 0;
+}
