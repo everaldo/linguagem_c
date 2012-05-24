3a0afdfadeb08cf766982931be3ea7185ed5603e
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-05-24 16:43:51 -0300

    exemplos de funcoes

---
+++ estrutura/identificadores/funcoes/alo_mundo_com_funcoes.c
@@ -0,0 +1,15 @@
+/* Everaldo 24/05/2012 */
+
+
+#include <stdio.h>
+
+void alo_mundo(){
+  printf("Alo mundo!\n");
+}
+
+int main(){
+  alo_mundo();
+
+  return 0;
+}
+
---
+++ estrutura/identificadores/funcoes/alo_mundo_com_funcoes_e_prototipo.c
@@ -0,0 +1,18 @@
+/* Everaldo 24/05/2012 */
+
+
+#include <stdio.h>
+
+void alo_mundo();
+
+
+int main(){
+  alo_mundo();
+
+  return 0;
+}
+
+void alo_mundo(){
+  printf("Alo mundo!\n");
+}
+
