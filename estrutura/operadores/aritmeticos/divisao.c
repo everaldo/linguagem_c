5be67390dfd5dc09825557e0b1fd36eea52f7197
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-05-24 12:09:02 -0300

    fundamentos da linguagem c - operadores

---
+++ estrutura/operadores/aritmeticos/divisao.c
@@ -0,0 +1,33 @@
+/* Everaldo Gomes - 24/05/2012 */
+
+#include <stdio.h>
+
+int main(){
+
+  printf("%d\n", 1 / 1);                //imprime 1
+
+  printf("%d\n", 1 / 2);                //imprime 0 - divisao inteira
+  printf("%f\n", 1.0 / 2);              //imprime 0.500000 - divisao float
+  printf("%f\n", 1 / 2.0);              //imprime 0.500000 - divisao float
+
+  printf("%f\n", 1.0 / 1e6);            //imprime 0.000001
+
+  printf("%f\n", 1.0 / 1e9);            //imprime 0.000000 -> mas o valor não é 0!!
+  printf("%.10f\n", 1.0 / 1e9);         //imprime 0.0000000010
+
+  printf("%f\n", 1.0 / 1e-9);           //imprime 1000000000.000000
+  printf("%e\n", 1.0 / 1e-9);           //imprime 1000000e+09
+
+  printf("%d\n", (int)1.0 / (int)1.0);  //imprime 1
+
+
+  printf("%f\n", 1.0 / 3.0);            //imprime 0.333333 -> mas o valor não é 1!!
+  printf("%e\n", 1e3 / 1e6);            //imprime 1.000000e-03
+  printf("%e\n", 1e6 / 1e-9);           //imprime 1.000000e+15
+  printf("%f\n", 1e2 / 1e-4);           //imprime 1000000.000000 
+
+  printf("%d\n", 726 / 11);             //imprime 66
+  printf("%c\n", 726 / 11);             //imprime B
+  printf("%c\n", 650 / 10);             //imprime A
+
+}
---
+++ estrutura/operadores/aritmeticos/divisao_por_zero.c
@@ -0,0 +1,19 @@
+/* Everaldo Gomes - 24/05/2012 */
+
+#include <stdio.h>
+
+int main(){
+
+  printf("%d\n", 1 / 0);    // Impossível dividir por zero
+  printf("Esta instrução nunca será executada - isso não será escrito na tela\n");
+
+
+/*
+gera warning (aviso na compilação)
+e erro em tempo de execução - programa termina e não executa
+as próximas instruções
+
+*/
+                    
+
+}
