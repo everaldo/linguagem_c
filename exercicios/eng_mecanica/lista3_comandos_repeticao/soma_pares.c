a03e53fcbd64867c5b10fd58a3dbd767f2beda97
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-05-28 14:25:07 -0300

    lista 3 - comandos repeticao

--- exercicios/eng_mecanica/lista3_comandos_repeticao/soma_pares.c
+++ exercicios/eng_mecanica/lista3_comandos_repeticao/soma_pares.c
@@ -1,1 +1,31 @@
-../../sala_aula/eng_mecanica/soma_pares.c
+/* Everaldo Gomes - 14/05/2012
+
+
+Exercício resolvido em sala de aula 
+(Eng. Mecânica - 14/05/2012 - 13h50 às 17h30)
+
+- Exercício número 1 da lista 3 - Comandos de Repetição
+
+Ler números inteiros do teclado e calcular 
+a soma dos que forem par. A leitura 
+deve parar quando o valor 0 (zero) for digitado
+
+*/
+
+#include <stdio.h>
+
+int main(){
+
+  int n, soma;
+
+  soma = 0;
+  scanf("%d",&n);
+  while( n != 0){ //soma números pares até ler o número zero
+    if (n % 2 == 0) //se o resto da divisão por 2 é igual a zero o número é par
+      soma += n; //não precisa colocar { e } porque é apenas um comando
+    scanf("%d", &n); //leia o próximo número
+  }
+  printf("%d\n", soma);
+
+  return 0;
+}
--- exercicios/eng_mecanica/lista3_comandos_repeticao/soma_pares_versao2.c
+++ exercicios/eng_mecanica/lista3_comandos_repeticao/soma_pares_versao2.c
@@ -1,1 +1,33 @@
-../../sala_aula/eng_mecanica/soma_pares_versao2.c
+/* Everaldo Gomes - 14/05/2012
+
+
+Exercício resolvido em sala de aula 
+(Eng. Mecânica - 14/05/2012 - 13h50 às 17h30)
+
+- Exercício número 1 da lista 3 - Comandos de Repetição
+
+Ler números inteiros do teclado e calcular 
+a soma dos que forem par. A leitura 
+deve parar quando o valor 0 (zero) for digitado
+
+*/
+
+#include <stdio.h>
+
+
+/* Esta versão usa o comando do{ }while(); */
+int main(){
+
+  int n, soma;
+
+  soma = 0;
+  do{
+    scanf("%d",&n);
+    if (n % 2 == 0) //se o resto da divisão por 2 é igual a zero o número é par
+      soma += n; //não precisa colocar { e } porque é apenas um comando
+
+  }while( n != 0); //soma números pares até ler o número zero
+  printf("%d\n", soma);
+
+  return 0;
+}
