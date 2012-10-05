86cd1ed83d4beafac38575bed9964d1a83768138
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-10-04 21:00:45 -0300

    nova versao calculadora

--- exercicios/automacao_industrial/calculadora/calculadora.c
+++ exercicios/automacao_industrial/calculadora/calculadora.c
@@ -1,68 +1,70 @@
-/* Everaldo Gomes - 31/05/2012 
-
-Calculadora
-
-Operações:
-
-1 - Soma
-2 - Subtracao
-3 - Multiplicacao
-4 - Divisao
-0 - Sair
-
-
-
-*/
-
+/* Inclusão de bibliotecas */
 #include <stdio.h>
 
+/* Definição de constantes */
+#define SOMA 1
+#define SUBTRACAO 2
+#define MULTIPLICACAO 3
+#define DIVISAO 4
+#define SAIR 0
+
+
+
+/* Programa Principal */
+
 int main(){
-  /* Declaração de variáveis */
-  int x, y, resultado, operacao;
+/* Declaração de variáveis */
+  int operacao, x, y, resultado;
+  char cod_operacao[] = { '+', '-' , '*', '/' } ;
 
 
   do{
+    /* Mensagem ao usuário*/
     puts("****Calculadora****");
     puts("1 - Soma"); 
     puts("2 - Subtração"); 
     puts("3 - Multiplicação"); 
     puts("4 - Divisão"); 
     puts("0 - Sair");
- 
 
+    /* Entrada de dados */
     scanf("%d", &operacao);
-
-    if(operacao == 1){
-      scanf("%d %d", &x, &y);
-     resultado = x + y;
-    } else if(operacao == 2){
-     scanf("%d %d", &x, &y);
-     resultado = x - y;
-   } else if(operacao == 3){
-     scanf("%d %d", &x, &y);
-     resultado = x * y;
-   } else if(operacao == 4){
-     scanf("%d %d", &x, &y);
-     if (y) resultado = x / y;
-     else puts("Divisao por zero");
-   } else if(operacao == 0){
-     return 0;
-   }
     
-    if(operacao) printf("Resultado = %d\n", resultado);
-
-  } while (operacao != 0);
-
-
-
-  /* Leitura dos operandos*/
- 
-
-  /* Execução da operação (cálculo)*/
+    if(operacao < 0 || operacao > 4){ //Reimprimir o menu quando a operacao é inválida
+      puts("Operação inválida");
+      continue;
+    }
+    if (!operacao){ //mesma coisa que operacao == 0
+      return 0;
+    }
 
 
-  /* Impressão do resultado */
+    puts("Digite os dois operandos");
+    scanf("%d %d", &x, &y);
 
-  return 0;
+
+
+    /* Cálculo (computação) */
+
+    switch (operacao){
+      case SOMA:
+        resultado = x + y;
+        break;
+      case SUBTRACAO:
+        resultado = x - y;
+        break;
+      case MULTIPLICACAO:
+        resultado = x * y;
+        break;
+      case DIVISAO:
+        if (y) resultado = x / y;
+        else puts("Divisao por zero");
+    }
+
+          /* Saída de dados (Resultado) */
+    if(operacao) printf("%d %c %d = %d\n", x, cod_operacao[operacao - 1] ,y, resultado);
+  }while (operacao);
+
 }
+
 
