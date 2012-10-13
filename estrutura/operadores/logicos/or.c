62a2874fbfffa55b194cc79c312c85ef7858d81a
everaldo <everaldo.gomes@gmail.com>
2012-10-13 14:47:09 -0300

    Update estrutura/operadores/logicos/or.c

--- estrutura/operadores/logicos/or.c
+++ estrutura/operadores/logicos/or.c
@@ -7,8 +7,8 @@
 int main(){
   
   printf("%d\n", 0 || 0);     //imprime 0
-  printf("%d\n", 0 || 1);     //imprime 0
-  printf("%d\n", 1 || 0);     //imprime 0 
+  printf("%d\n", 0 || 1);     //imprime 1
+  printf("%d\n", 1 || 0);     //imprime 1 
   printf("%d\n", 1 || 1);     //imprime 1
 
 
