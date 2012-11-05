655217992af99835e969c48a8bc14a9fe594f5a4
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-11-05 16:05:00 -0200

    palindromo - em produção

---
+++ exercicios/sala_aula/eng_mecanica/5novembro/palindromo.c
@@ -0,0 +1,52 @@
+/* Everaldo Gomes - 05/11/2012
+ *
+ * Programa verifica se uma string é palíndromo
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+void  le_string(char **s);
+void  eh_palindromo(char *s, char **r);
+void  imprime_resultado(char *r);
+
+
+int main(void){
+  char s[MAX + 1], r[4];
+
+  le_string(&s);
+  eh_palindromo(s,&r);
+  imprime_resultado(r);
+  return 0;
+}
+
+void  le_string(char **s){
+  scanf("%s", *s);
+
+}
+
+void  eh_palindromo(char *s, char **r){
+  int palindromo = 1; //assume que é palindromo
+  int i, ultimo;
+
+  for(i = 0, ultimo = strlen(s) - 1; s[i] == s[ultimo]
+      && i <= ultimo; i++, ultimo--);
+
+  if (i < ultimo) palindromo = 0;
+
+  if (palindromo) *r = strdup("sim");
+  else *r = strdup("não");
+
+}
+
+void  imprime_resultado(char *r){
+  printf("%s", r);
+}
+
+
+
+
