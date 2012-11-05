ecf6a424e66dbf13e66ad169bd133c517df5a88b
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-11-05 16:56:22 -0200

    versao 2 palindromo

--- exercicios/sala_aula/eng_mecanica/5novembro/palindromo.c
+++ exercicios/sala_aula/eng_mecanica/5novembro/palindromo.c
@@ -16,26 +16,56 @@ void  imprime_resultado(char *r);
 
 
 int main(void){
-  char s[MAX + 1], *r;
-  s[0] = '\0';
-  le_string(&s);
-  eh_palindromo(s,&r);
+  char palavra[MAX + 1], *r, *p;
+  palavra[0] = '\0';
+  p = &palavra[0];
+  le_string(&p);
+  eh_palindromo(p,&r);
   imprime_resultado(r);
   return 0;
 }
 
 void  le_string(char **s){
-  scanf("%s", s);
+  puts("Digite uma palavra para verificar se é palíndromo");
+  scanf("%s", *s);
 
 }
 
-void  eh_palindromo(char *s, char **r){
-  int palindromo = 1; //assume que é palindromo
-  int i, ultimo;
+void eh_palindromo(char *s, char**r){
+  char *primeiro, *ultimo;
+  int palindromo = 1;
 
-  for(i = 0, ultimo = strlen(s) - 1; ultimo != -1 &&
+  primeiro = &s[0];
+  ultimo = &s[strlen(s) - 1];
+
+  while( *primeiro == *ultimo ){
+    primeiro++;
+    ultimo--;
+
+  }
+
+  if (primeiro < ultimo) palindromo = 0;
+
+  if (palindromo) *r = strdup("sim");
+  else *r = strdup("não");
+
+
+
+
+}
+
+
+/*void  eh_palindromo(char *s, char **r){
+  int palindromo = 1; //assume que é palindromo
+  int i = 0;
+  int ultimo = strlen(s) - 1;
+
+  while(ultimo != -1 &&
       s[i] == s[ultimo]
-      && i <= ultimo; i++, ultimo--);
+      && i <= ultimo ){
+    i++;
+    ultimo--;
+  }
 
   if (i < ultimo) palindromo = 0;
   if(ultimo == -1) palindromo = 1;
@@ -44,7 +74,7 @@ void  eh_palindromo(char *s, char **r){
   if (palindromo) *r = strdup("sim");
   else *r = strdup("não");
 
-}
+}*/
 
 void  imprime_resultado(char *r){
   printf("%s\n", r);
