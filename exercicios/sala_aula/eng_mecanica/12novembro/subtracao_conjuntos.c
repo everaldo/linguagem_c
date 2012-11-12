3996df43a62d8a30d5050a1579bf8dd789f25228
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-11-12 16:24:32 -0200

    le e imprime vetor

--- exercicios/sala_aula/eng_mecanica/12novembro/subtracao_conjuntos.c
+++ exercicios/sala_aula/eng_mecanica/12novembro/subtracao_conjuntos.c
@@ -12,10 +12,10 @@
 
 #define MAX 10
 
-void le_vetor(int *v, int *tam);
-void imprime_vetor(int *v, int *n);
+void le_vetor(int *v, int *n);
+void imprime_vetor(int *v, int n);
 void interseccao(int *v, int n, int *inter, int *tam);
-void remove(int* v, int *n, int x);
+void remover(int* v, int *n, int x);
 void subtracao(int *x, int n, int *y, int m, int* s, int* tam);
 
 
@@ -24,9 +24,46 @@ int main(void){
 
   le_vetor(a, &n);
   le_vetor(b, &m);
-  subtracao(a, n, b, m, sub, &tam);
-  imprime_vetor(sub, tam);
+  imprime_vetor(a, n);
+  imprime_vetor(b, m);
+//  subtracao(a, n, b, m, sub, &tam);
+//  imprime_vetor(sub, tam);
 
   return 0;
 }
+
+
+void le_vetor(int *v, int *n){
+  int i;
+  puts("Digite o tamanho do vetor");
+  scanf("%d", n);
+
+  puts("Digite os elementos do vetor");
+  for(i = 0; i < *n; i++){
+    scanf("%d", &v[i]);
+  }
+
+}
+
+
+
+void imprime_vetor(int *v, int n){
+  int i;
+  printf("[");
+
+  for(i = 0; i < n - 1; i++){
+    printf("%d, ", v[i]);
+  }
+  printf("%d]\n", v[n - 1]);
+}
+
+void interseccao(int *v, int n, int *inter, int *tam){}
+void remover(int* v, int *n, int x){}
+void subtracao(int *x, int n, int *y, int m, int* s, int* tam){}
+
+
+
+
+
+
 
