fe20e422b295550082f94dd61721e283ce5fdc7b
Everaldo Gomes <everaldo.gomes@gmail.com>
2012-12-20 19:50:27 -0200

    arrumando mensagem

--- estrutura/operadores/aritmeticos/divisao.c
+++ estrutura/operadores/aritmeticos/divisao.c
@@ -16,7 +16,7 @@ int main(){
   printf("%.10f\n", 1.0 / 1e9);         //imprime 0.0000000010
 
   printf("%f\n", 1.0 / 1e-9);           //imprime 1000000000.000000
-  printf("%e\n", 1.0 / 1e-9);           //imprime 1000000e+09
+  printf("%e\n", 1.0 / 1e-9);           //imprime 1.000000e+09
 
   printf("%d\n", (int)1.0 / (int)1.0);  //imprime 1
 
@@ -24,7 +24,7 @@ int main(){
   printf("%f\n", 1.0 / 3.0);            //imprime 0.333333 -> mas o valor não é 1!!
   printf("%e\n", 1e3 / 1e6);            //imprime 1.000000e-03
   printf("%e\n", 1e6 / 1e-9);           //imprime 1.000000e+15
-  printf("%f\n", 1e2 / 1e-4);           //imprime 1000000.000000 
+  printf("%f\n", 1e2 / 1e-4);           //imprime 1000000.000000
 
   printf("%d\n", 726 / 11);             //imprime 66
   printf("%c\n", 726 / 11);             //imprime B
