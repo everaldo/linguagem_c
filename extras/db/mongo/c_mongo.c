74fa11fb95e1f95ac32bf47fe778522154ede0da
Pedro Henrique (@phinfonet) <pedrohenriqueacruz@gmail.com>
2014-03-22 20:19:32 -0300

    started c_mongo

---
+++ extras/db/mongo/c_mongo.c
@@ -0,0 +1,11 @@
+/* 22/03/2014 - Pedro Henrique A. Cruz - @phinfonet
+    Exemplo de C e mongoDB
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
