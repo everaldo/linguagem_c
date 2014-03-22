b74e413fe7a5354d5ab044a8394c0d0469fb6f73
Pedro Henrique (@phinfonet) <pedrohenriqueacruz@gmail.com>
2014-03-22 20:54:20 -0300

    added mac instruction to readme

--- extras/db/mongo/README.md
+++ extras/db/mongo/README.md
@@ -1,2 +1,6 @@
-# C and MongoDB #
+C and MongoDB
+=========
+
+Firstly you need to install and build the Mongo Driver:
+  brew install mongo-c
 
--- extras/db/mongo/c_mongo.c
+++ extras/db/mongo/c_mongo.c
@@ -3,9 +3,22 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+// header for c-mongo lib
+#include <mongo.h>
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  mongo conn[1];
+  int status = mongo_client( conn, "127.0.0.1", 27017 );
+
+  if( status != MONGO_OK ) {
+      switch ( conn->err ) {
+        case MONGO_CONN_NO_SOCKET:  printf( "no socket\n" ); return 1;
+        case MONGO_CONN_FAIL:       printf( "connection failed\n" ); return 1;
+        case MONGO_CONN_NOT_MASTER: printf( "not master\n" ); return 1;
+      }
+  }
+
+  mongo_destroy( conn );
   return 0;
 }
